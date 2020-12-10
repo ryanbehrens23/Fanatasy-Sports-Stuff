27466662c9278f6b1da3ba127efc3249613f619e
Ryan Behrens <47507405+ryanbehrens23@users.noreply.github.com>
2020-12-10 11:27:30 -0600

    Update FantasyGenerator.java

--- randomfuntraining/fantasySports/FantasyGenerator.java
+++ randomfuntraining/fantasySports/FantasyGenerator.java
@@ -1,44 +1,96 @@
-package com.deere.JavaTraining.randomfuntraining.fantasySports;
+package randomfuntraining.fantasySports.fantasySports;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class FantasyGenerator {
-    private final static int[] draftOrder = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
     private final static String[] teams = new String[]{"Penetrate Down Low", "Barrett-tone A-Capela", "Rim Jobs", "Kawhi not Rubio On my Gasol",
             "All-STAR Ball handlers", "I Get Injured", "KAT in the Hat", "The Monstars", "Chitown BallHogs",
             "Hillbilly Hickdeads", "Dirty Pascal", "Men's Butts Drive Me Nuts"};
+    private final static String[] draftOrder = new FantasyGenerator().printDraftOrder(teams);
+    private final static int[] number = new int[]{1,2,3,4,5,6,7,8,9,10,11,12};
     public static void main(String[] args){
 
         List<Teams> teamsList = getFantasyBasketballTeams();
         Collections.shuffle(teamsList);
         printTeamsList(teamsList);
 
-        Random random = new Random();
-
-        printDraftOrder(teams[random.nextInt(teams.length)]);
+//        Random random = new Random();
+//
+//        System.out.println("Pick #" + Arrays.toString(draftOrder) + " goes to " + Arrays.toString(printDraftOrder(teams)));
 
         System.out.println("Bud Division: \n" + teamsList.subList(0, 6));
         System.out.println("Miller Division: \n" + teamsList.subList(6, 12));
     }
 
-    public static void printDraftOrder(String team) {
-        Random random = new Random();
-        String team1 = teams[teams.length];
-        String team2 = teams[teams.length];
-        for (int i = 1; i < draftOrder.length; i++){
-            System.out.println("Pick #" + draftOrder[i] + " goes to " + teams[random.nextInt(teams.length)]);
+    public static String[] printDraftOrder(String[] team) {
+        // for string array name is null then return null
+        if (team == null) return null;
+        // for string array name length is zero then return an empty string array
+        if (team.length == 0) return new String[0];
+        // for single element in the array return the same array
+        if (team.length == 1) return team;
+
+        int teamLength = team.length;
+
+        Boolean[] chosen = new Boolean[teamLength];
+        Boolean[] drafter = new Boolean[teamLength];
+
+        // initialize arrays with false
+        Arrays.fill(chosen, false);
+        Arrays.fill(drafter, false);
+
+        Random rand = new Random();
+        // final result array
+        String[] result = new String[team.length];
+        int position = 0;
+
+        while (true) {
+            int randomInt1 = rand.nextInt(teamLength);
+            int randomInt2 = rand.nextInt(teamLength);
+
+            // get random person
+            String person1 = team[randomInt1];
+            String person2 = team[randomInt2];
+
+            if (person1 == person2) {
+                // do nothing
+            }
+            // if two person name are not same
+            if ((!person1.equals(person2)) && (drafter[randomInt1] == false)
+                    && (chosen[randomInt2] == false)) {
+
+                if (!person1.equals(team[position])) {
+                    result[position] = person1;
+                    position++;
+
+                    drafter[randomInt1] = true;
+                    chosen[randomInt2] = true;
+
+                    if (isArrCompleted(drafter) && isArrCompleted(chosen)) break;
+                }
+
+            }
         }
+        return result;
+    }
+
+    private static boolean isArrCompleted(Boolean[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == false) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static void printTeamsList(List<Teams> teamsList) {
-        for (Teams teams : teamsList){
-            for (int order = 0; order <=12; order++)
-                System.out.println("Pick #" + draftOrder[] teams);
+        for (int order = 0; order < teams.length; order++) {
+            System.out.println("Pick #" + number[order] + " goes to " + draftOrder[order]);
+            if (draftOrder[order].equals(teams[order]))
+                System.out.println("ERROR: " + teams[order]);
         }
     }
+
 
     public static List<Teams> getFantasyBasketballTeams() {
         List<Teams> teamsList = new ArrayList<>();
