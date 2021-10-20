4374b84bd9f264fef7bb4bb5c6647ee7886139ba
Ryan Behrens <xlilfellerx@live.com>
2021-10-20 13:23:47 -0500

    Pull Request with updated code

--- randomfuntraining/fantasySports/FantasyGenerator.java
+++ randomfuntraining/fantasySports/FantasyGenerator.java
@@ -6,7 +6,7 @@ public class FantasyGenerator {
     private final static String[] teams = new String[]{"Penetrate Down Low", "Barrett-tone A-Capela", "Rim Jobs", "Kawhi not Rubio On my Gasol",
             "All-STAR Ball handlers", "I Get Injured", "KAT in the Hat", "The Monstars", "Chitown BallHogs",
             "Hillbilly Hickdeads", "Dirty Pascal", "Men's Butts Drive Me Nuts"};
-    private final static String[] draftOrder = new FantasyGenerator().printDraftOrder(teams);
+    private final static String[] draftOrder = FantasyGenerator.printDraftOrder(teams);
     private final static int[] number = new int[]{1,2,3,4,5,6,7,8,9,10,11,12};
     public static void main(String[] args){
 
