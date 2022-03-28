cf1c9f669daeb6418129377f024e37803fd7ed31
Ryan Behrens <47507405+ryanbehrens23@users.noreply.github.com>
2022-03-28 10:54:12 -0500

    Create GolfTeams.java in Java directory

---
+++ com/opiandthebear/java/GolfTeams.java
@@ -0,0 +1,232 @@
+package randomfuntraining.fantasySports;
+
+import java.util.*;
+
+public class FantasyGenerator {
+    private final static String[] teams = new String[]{""};
+    private final static String[] draftOrder = new FantasyGenerator().printDraftOrder(teams);
+    private final static int[] number = new int[]{1,2,3,4,5,6,7,8,9,10,11,12};
+    private final static String[] bud = new String[]{""};
+    private final static String[] miller = new String[]{""};
+    private final static String[] teamA = new String[]{""};
+    private final static String[] teamB = new String[]{""};
+    private final static String[] teamC = new String[]{""};
+    private final static String[] teamD = new String[]{""};
+    public static void main(String[] args){
+        //Creates teamList list from the teams listed teams String Array
+//        List<Teams> teamsList = getFantasyBasketballTeams();
+//        Collections.shuffle(teamsList);
+//        printTeamsList(teamsList);
+
+//        Random random = new Random();
+
+        //Prints out draft order
+//        System.out.println("Pick #" + Arrays.toString(draftOrder) + " goes to " + Arrays.toString(printDraftOrder(teams)));
+
+        //This prints out separate divisions
+//        System.out.println("Bud Division: \n" + teamsList.subList(0, 6));
+//        System.out.println("Miller Division: \n" + teamsList.subList(6, 12));
+
+        //This method is used to choose teams head to head
+        //getH2HSingles();
+        getH2HDoules();
+        //getH2HTeams();
+    }
+
+    public static void getH2HSingles() {
+        Collections.shuffle(Arrays.asList(teamA));
+        Collections.shuffle(Arrays.asList(teamB));
+        Boolean[] teamAChosen = new Boolean[teamA.length];
+        Boolean[] teamBChosen = new Boolean[teamB.length];
+        Arrays.fill(teamAChosen, false);
+        Arrays.fill(teamBChosen, false);
+        int position = 0;
+        while (true) {
+            Random rand = new Random();
+            int randomInt1 = rand.nextInt(teamA.length);
+            int randomInt2 = rand.nextInt(teamB.length);
+            // get random person
+            String person1 = teamA[randomInt1];
+            String person2 = teamB[randomInt2];
+            for (int week = 1; week <=6; week++) {
+                if (!person1.equals(person1) && !person2.equals(person2) && budChosen[randomInt1] == false && millerChosen[randomInt2] == false) {
+                    if (!person1.equals(teamA[position]) && !person1.equals(teamB[position])) {
+                        System.out.println(teamA[position] + " vs " + teamB[position]);
+                        position++;
+                        teamAChosen[randomInt1] = true;
+                        teamBChosen[randomInt2] = true;
+                    }
+                    if (isArrCompleted(teamAChosen) && isArrCompleted(teamBChosen)) break;
+                }
+            }
+        }
+    }
+
+    public static void getH2HDoubles() {
+        Collections.shuffle(Arrays.asList(teamA));
+        Collections.shuffle(Arrays.asList(teamB));
+        Collections.shuffle(Arrays.asList(teamC));
+        Collections.shuffle(Arrays.asList(teamD));
+        Boolean[] teamAChosen = new Boolean[teamA.length];
+        Boolean[] teamBChosen = new Boolean[teamB.length];
+        Boolean[] teamCChosen = new Boolean[teamC.length];
+        Boolean[] teamDChosen = new Boolean[teamD.length];
+        Arrays.fill(teamAChosen, false);
+        Arrays.fill(teamBChosen, false);
+        Arrays.fill(teamCChosen, false);
+        Arrays.fill(teamDChosen, false);
+        int position = 0;
+        while (true) {
+            Random rand = new Random();
+            int randomInt1 = rand.nextInt(teamA.length);
+            int randomInt2 = rand.nextInt(teamB.length);
+            int randomInt3 = rand.nextInt(teamC.length);
+            int randomInt4 = rand.nextInt(teamD.length);
+            // get random person
+            String person1 = teamA[randomInt1];
+            String person2 = teamB[randomInt2];
+            String person3 = teamC[randomInt3];
+            String person4 = teamD[randomInt4];
+            for (int week = 1; week <= 6; week++) {
+                if (!person1.equals(person1) && !person2.equals(person2) && !person3.equals(person3) && !person4.equals(person4) && teamAChosen[randomInt1] == false && teamBChosen[randomInt2] == false && teamCChosen[randomInt3] == false && teamDChosen[randomInt4] == false) {
+                    if (!person1.equals(teamA[position]) && !person1.equals(teamB[position]) && !person1.equals(teamC[position]) && !person1.equals(teamD[position])) {
+                        System.out.println(teamA[]+" vs " + teamB[]);
+                        System.out.println(teamC[]+" vs " + teamD[]);
+                        position++;
+                        teamAChosen[randomInt1] = true;
+                        teamBChosen[randomInt2] = true;
+                        teamCChosen[randomInt3] = true;
+                        teamDChosen[randomInt4] = true;
+                    }
+                    if (isArrCompleted(teamAChosen) && isArrCompleted(teamBChosen) && isArrCompleted(teamCChosen) && isArrCompleted(teamDChosen))
+                        break;
+                }
+            }
+        }
+    }
+    public static void getH2HTeams() {
+        Collections.shuffle(Arrays.asList(bud));
+        Collections.shuffle(Arrays.asList(miller));
+        Boolean[] budChosen = new Boolean[bud.length];
+        Boolean[] millerChosen = new Boolean[miller.length];
+        Arrays.fill(budChosen, false);
+        Arrays.fill(millerChosen, false);
+        int position = 0;
+        while (true) {
+            Random rand = new Random();
+            int randomInt1 = rand.nextInt(bud.length);
+            int randomInt2 = rand.nextInt(miller.length);
+            // get random person
+            String person1 = bud[randomInt1];
+            String person2 = miller[randomInt2];
+            if (!person1.equals(person1) && !person2.equals(person2) && budChosen[randomInt1] == false && millerChosen[randomInt2] == false) {
+                if (!person1.equals(bud[position]) && !person1.equals(miller[position])) {
+                    System.out.println(bud[position] + " vs " + miller[position]);
+                    position++;
+                    budChosen[randomInt1] = true;
+                    millerChosen[randomInt2] = true;
+                }
+                if (isArrCompleted(budChosen) && isArrCompleted(millerChosen)) break;
+            }
+        }
+    }
+    public String[] printDraftOrder(String[] team) {
+        // for string array name is null then return null
+        if (team == null) return null;
+        // for string array name length is zero then return an empty string array
+        if (team.length == 0) return new String[0];
+        // for single element in the array return the same array
+        if (team.length == 1) return team;
+
+        int teamLength = team.length;
+
+        Boolean[] chosen = new Boolean[teamLength];
+        Boolean[] drafter = new Boolean[teamLength];
+
+        // initialize arrays with false
+        Arrays.fill(chosen, false);
+        Arrays.fill(drafter, false);
+
+        Random rand = new Random();
+        // final result array
+        String[] result = new String[team.length];
+        int position = 0;
+
+        while (true) {
+            int randomInt1 = rand.nextInt(teamLength);
+            int randomInt2 = rand.nextInt(teamLength);
+
+            // get random person
+            String person1 = team[randomInt1];
+            String person2 = team[randomInt2];
+
+            if (person1 == person2) {
+                // do nothing
+            }
+            // if two person name are not same
+            if ((!person1.equals(person2)) && (drafter[randomInt1] == false)
+                    && (chosen[randomInt2] == false)) {
+
+                if (!person1.equals(team[position])) {
+                    result[position] = person1;
+                    position++;
+
+                    drafter[randomInt1] = true;
+                    chosen[randomInt2] = true;
+
+                    if (isArrCompleted(drafter) && isArrCompleted(chosen)) break;
+                }
+
+            }
+        }
+        return result;
+    }
+
+    private static boolean isArrCompleted(Boolean[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == false) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void printTeamsList(List<Teams> teamsList) {
+        for (int order = 0; order < teams.length; order++) {
+            System.out.println("Pick #" + number[order] + " goes to " + draftOrder[order]);
+            if (draftOrder[order].equals(teams[order]))
+                System.out.println("ERROR: " + teams[order]);
+        }
+    }
+
+
+    public static List<Teams> getFantasyBasketballTeams() {
+        List<Teams> teamsList = new ArrayList<>();
+
+        Teams one = new Teams("");
+        teamsList.add(one);
+        Teams two = new Teams("");
+        teamsList.add(two);
+        Teams three = new Teams("");
+        teamsList.add(three);
+        Teams four = new Teams("l");
+        teamsList.add(four);
+        Teams five = new Teams("");
+        teamsList.add(five);
+        Teams six = new Teams("");
+        teamsList.add(six);
+        Teams seven = new Teams("");
+        teamsList.add(seven);
+        Teams eight = new Teams("");
+        teamsList.add(eight);
+        Teams nine = new Teams("");
+        teamsList.add(nine);
+        Teams ten = new Teams("");
+        teamsList.add(ten);
+        Teams eleven = new Teams("");
+        teamsList.add(eleven);
+        Teams tweleve = new Teams("");
+        teamsList.add(tweleve);
+        return teamsList;
+    }
+}
